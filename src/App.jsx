7505c1cb2efa9d3adae711978032abc0260a6d2e
unknown <notnotsky26u@gmaile.com>
2025-04-08 09:47:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="w-screen h-screen flex flex-col" id="container">
+      <div className="flex" id="headingContaine">
+        <h1
+          className="font-bold text-6xl break-all max-w-[200px] text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-pink-400"
+          id="headingText"
+        >
+          Hi ,im skanda
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
